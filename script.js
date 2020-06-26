54f0a2e8ae535b71ab501daebb01cb6c7e8b6076
Andi Qu <andi@andiqu.com>
2020-06-26 16:35:18 +0200

    Added more pickup line options

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ pickup_opts = [
     "Can I borrow a quarter? Because",
     "You're hot, but",
     "Excuse me, sexy, but",
-    "Do you have a twin sister? Because"
+    "Do you have a twin sister? Because",
+    "Hey honey. Are you from Tennessee? Because",
+    "Words alone can't describe how you look, but",
+    "Can you say Constantinople backwards? Me neither, but",
+    "Excuse me, I just noticed",
+    "Hi, the voices in my head told me",
+    "Are you the Higgs Boson? Because",
+    "Baby, are a broom? Because",
 ];
 
 fetch(
@@ -25,6 +32,14 @@ fetch(
             pickup_opts[Math.floor(Math.random() * pickup_opts.length)];
 
         let idx = Math.floor(Math.random() * 3);
-        document.getElementById("verse").innerHTML =
-            `${json[idx][Math.random() > 0.9 ? "textOriginal" : "text"]} (${json[idx]["reference"]})`;
+
+        let verse = `${
+            json[idx][Math.random() > 0.9 ? "textOriginal" : "text"]
+        } (${json[idx]["reference"]})`;
+        if (verse.slice(0, 3) === "But") verse = verse.slice(3);
+        if (verse.slice(0, 3) === "And") verse = verse.slice(3);
+        if (verse.slice(0, 3) === "For") verse = verse.slice(3);
+        if (verse.slice(0, 7) === "Because") verse = verse.slice(7);
+
+        document.getElementById("verse").innerHTML = verse;
     });
